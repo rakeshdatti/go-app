c2840b70dbe8e26d4addf70a18d9c6c2e194f8ad
rakeshdatti <rakeshdatti05@gmail.com>
2025-06-20 18:50:41 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Dockerfile
+FROM golang:1.20-alpine
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+
+RUN go build -o app .
+
+EXPOSE 8080
+
+CMD ["./app"]
+
---
+++ main.go
@@ -0,0 +1,22 @@
+// main.go
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome to Go App in Jenkins CI/CD!")
+	})
+
+	router.Run(":8080")
+}
+
